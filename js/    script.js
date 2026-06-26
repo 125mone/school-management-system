246a0992bc423c1902f7e0ad11471a9b42b5c741
125mone <dkg6krdcyz@privaterelay.appleid.com>
2026-06-26 18:09:53 +0000

    Update home page

---
+++ js/    script.js
@@ -0,0 +1,21 @@
+console.log("School System Loaded");
+
+document.querySelectorAll("button")[0].onclick = function () {
+    alert("دخول الوزارة قريباً");
+};
+
+document.querySelectorAll("button")[1].onclick = function () {
+    alert("دخول المدرسة قريباً");
+};
+
+document.querySelectorAll("button")[2].onclick = function () {
+    alert("دخول الطالب قريباً");
+};
+
+document.querySelectorAll("button")[3].onclick = function () {
+    alert("النتائج قريباً");
+};
+
+document.querySelectorAll("button")[4].onclick = function () {
+    alert("الأوائل قريباً");
+};
